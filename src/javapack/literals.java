b5c82c72c6cef3426012d624741e7d05775c0538
Vishnu-Girish <unhackedvish@gmail.com>
2025-05-04 22:45:15 +0530

    added literals code

---
+++ src/javapack/literals.java
@@ -0,0 +1,32 @@
+package javapack;
+
+public class literals {
+	public static void main(String[] args) {
+		int decimal = 10;
+		int binary = 0b1100100;
+		int octal = 0144;
+		int hex = 0x64;
+		
+		float floatx = 3.14f;
+		double doublex = 2.171828;
+		
+		char charx = 'A';
+		
+		boolean bT = true;
+		boolean bF = false;
+		
+		System.out.println("Interger literals:");
+		System.out.println("decimal:" + decimal);
+		System.out.println("binary" + binary);
+		System.out.println("octal" + octal);
+		System.out.println("hexadecimal" + hex);
+		System.out.println("Float literal");
+		System.out.println("float:" + floatx);
+		System.out.println("double:" + doublex);
+		System.out.println("Character literals:");
+		System.out.println("character:" + charx);
+		System.out.println("Boolean literals;");
+		System.out.println("boolean true:" + bT);
+		System.out.println("booleam false" + bF);
+	}
+}
